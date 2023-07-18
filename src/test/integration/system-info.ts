ecee9233477f977b9aed388091d64d6364fed580
thenhatat <nhat0103st@gmail.com>
2023-07-18 14:28:49 +0700

    add object to interact with chain

---
+++ src/test/integration/system-info.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai"
+import { config } from "dotenv"
+import {CheckersStargateClient} from "../../checkers_stargateclient";
+import {CheckersExtension} from "../../modules/queries";
+import _ from "../../../environment"
+
+config()
+
+describe("SystemInfo", function () {
+    let client: CheckersStargateClient, checkers: CheckersExtension["checkers"]
+
+    before("create client", async function () {
+        client = await CheckersStargateClient.connect(process.env.RPC_URL)
+        checkers = client.checkersQueryClient!.checkers
+    })
+
+    it("can get system info", async function () {
+        const systemInfo = await checkers.getSystemInfo()
+        expect(systemInfo.nextId.toNumber()).to.be.greaterThanOrEqual(1)
+        expect(parseInt(systemInfo.fifoHeadIndex, 10)).to.be.greaterThanOrEqual(-1)
+        expect(parseInt(systemInfo.fifoTailIndex, 10)).to.be.greaterThanOrEqual(-1)
+    })
+})
