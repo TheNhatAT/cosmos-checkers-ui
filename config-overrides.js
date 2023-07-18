1f7f6cf971884908c1feffdf10fb47c8f5d7950c
thenhatat <nhat0103st@gmail.com>
2023-07-18 16:51:41 +0700

    add query games

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const webpack = require("webpack")
+
+module.exports = function override(config, env) {
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+        new webpack.EnvironmentPlugin(["RPC_URL"]),
+        new webpack.EnvironmentPlugin(["FAUCET_URL"]),
+    )
+    config.resolve.fallback = {
+        buffer: false,
+        crypto: false,
+        events: false,
+        path: false,
+        stream: false,
+        string_decoder: false,
+        assert: false,
+    }
+    return config
+}
